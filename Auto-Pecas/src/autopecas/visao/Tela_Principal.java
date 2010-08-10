cee7c20c89969ae8483554a450bcf39a179213b0
caitanojunior <caitanojunior@acc6db73-e5c7-5ca1-e75e-2edd091f8931>
2010-08-10 20:03:24 +0000

    Criação da tela cadastro de produtos e melhoramento da tela principal quem baixar a primeira vez vai ter que instlaar o plugin timerbean me mandem email pra eu explicar como proceder
    Pode executar que funciona.

--- Auto-Pecas/src/autopecas/visao/Tela_Principal.java
+++ Auto-Pecas/src/autopecas/visao/Tela_Principal.java
@@ -42,14 +42,14 @@ public class Tela_Principal extends javax.swing.JFrame {
         timer1 = new org.netbeans.examples.lib.timerbean.Timer();
         jPanel1 = new javax.swing.JPanel();
         jButton_produtos = new javax.swing.JButton();
-        jButton_clientes = new javax.swing.JButton();
         jButton_sair = new javax.swing.JButton();
+        jButton1 = new javax.swing.JButton();
         jLabel_hora = new javax.swing.JLabel();
         jLabel_data = new javax.swing.JLabel();
         jMenuBar1 = new javax.swing.JMenuBar();
         jMenu_cadastros = new javax.swing.JMenu();
-        jMenuItem1 = new javax.swing.JMenuItem();
-        jMenuItem2 = new javax.swing.JMenuItem();
+        jMenuItem_cad_produtos = new javax.swing.JMenuItem();
+        jMenuItem_clientes = new javax.swing.JMenuItem();
         jMenu_sair = new javax.swing.JMenu();
 
         timer1.addTimerListener(new org.netbeans.examples.lib.timerbean.TimerListener() {
@@ -59,22 +59,23 @@ public class Tela_Principal extends javax.swing.JFrame {
         });
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setTitle("Auto Peças");
 
-        jPanel1.setBackground(new java.awt.Color(0, 204, 51));
+        jPanel1.setBorder(javax.swing.BorderFactory.createEtchedBorder());
 
-        jButton_produtos.setFont(new java.awt.Font("Arial", 0, 14)); // NOI18N
-        jButton_produtos.setText("Produtos");
+        jButton_produtos.setFont(new java.awt.Font("Arial", 0, 12)); // NOI18N
+        jButton_produtos.setText("Entrada de Material");
 
-        jButton_clientes.setFont(new java.awt.Font("Arial", 0, 14)); // NOI18N
-        jButton_clientes.setText("Clientes");
-
-        jButton_sair.setFont(new java.awt.Font("Arial", 0, 14)); // NOI18N
+        jButton_sair.setFont(new java.awt.Font("Arial", 0, 12)); // NOI18N
         jButton_sair.setText("Sair");
         jButton_sair.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jButton_sairActionPerformed(evt);
             }
         });
+
+        jButton1.setFont(new java.awt.Font("Arial", 0, 12)); // NOI18N
+        jButton1.setText("Vendas");
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
@@ -83,43 +84,55 @@ public class Tela_Principal extends javax.swing.JFrame {
             .addGroup(jPanel1Layout.createSequentialGroup()
                 .addContainerGap()
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jButton_produtos, javax.swing.GroupLayout.DEFAULT_SIZE, 147, Short.MAX_VALUE)
-                    .addComponent(jButton_sair, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, 147, Short.MAX_VALUE)
-                    .addComponent(jButton_clientes, javax.swing.GroupLayout.DEFAULT_SIZE, 147, Short.MAX_VALUE))
+                    .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 147, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jButton_produtos, javax.swing.GroupLayout.PREFERRED_SIZE, 147, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jButton_sair, javax.swing.GroupLayout.DEFAULT_SIZE, 147, Short.MAX_VALUE))
                 .addContainerGap())
         );
+
+        jPanel1Layout.linkSize(javax.swing.SwingConstants.HORIZONTAL, new java.awt.Component[] {jButton1, jButton_produtos});
+
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
                 .addGap(41, 41, 41)
                 .addComponent(jButton_produtos, javax.swing.GroupLayout.PREFERRED_SIZE, 37, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(29, 29, 29)
-                .addComponent(jButton_clientes)
-                .addGap(27, 27, 27)
+                .addGap(18, 18, 18)
+                .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 34, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
                 .addComponent(jButton_sair)
-                .addContainerGap(371, Short.MAX_VALUE))
+                .addContainerGap(387, Short.MAX_VALUE))
         );
 
-        jPanel1Layout.linkSize(javax.swing.SwingConstants.VERTICAL, new java.awt.Component[] {jButton_clientes, jButton_produtos, jButton_sair});
+        jPanel1Layout.linkSize(javax.swing.SwingConstants.VERTICAL, new java.awt.Component[] {jButton1, jButton_produtos, jButton_sair});
 
         jLabel_hora.setText("hora");
 
         jLabel_data.setText("data");
 
+        jMenuBar1.setBackground(new java.awt.Color(0, 204, 51));
+
+        jMenu_cadastros.setBackground(new java.awt.Color(0, 204, 0));
         jMenu_cadastros.setText("Cadastros");
 
-        jMenuItem1.setFont(new java.awt.Font("Arial", 0, 14)); // NOI18N
-        jMenuItem1.setIcon(new javax.swing.ImageIcon("C:\\Documents and Settings\\Kelly\\Meus documentos\\NetBeansProjects\\Auto-Pecas\\imagens\\botoes\\produto.gif")); // NOI18N
-        jMenuItem1.setText("Produtos");
-        jMenu_cadastros.add(jMenuItem1);
+        jMenuItem_cad_produtos.setFont(new java.awt.Font("Arial", 0, 14)); // NOI18N
+        jMenuItem_cad_produtos.setIcon(new javax.swing.ImageIcon("C:\\Documents and Settings\\Kelly\\Meus documentos\\NetBeansProjects\\Auto-Pecas\\imagens\\botoes\\produto.gif")); // NOI18N
+        jMenuItem_cad_produtos.setText("Cadastro de Produtos");
+        jMenuItem_cad_produtos.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mousePressed(java.awt.event.MouseEvent evt) {
+                jMenuItem_cad_produtosMousePressed(evt);
+            }
+        });
+        jMenu_cadastros.add(jMenuItem_cad_produtos);
 
-        jMenuItem2.setFont(new java.awt.Font("Arial", 0, 14)); // NOI18N
-        jMenuItem2.setIcon(new javax.swing.ImageIcon("C:\\Documents and Settings\\Kelly\\Meus documentos\\NetBeansProjects\\Auto-Pecas\\imagens\\botoes\\cliente.gif")); // NOI18N
-        jMenuItem2.setText("Clientes");
-        jMenu_cadastros.add(jMenuItem2);
+        jMenuItem_clientes.setFont(new java.awt.Font("Arial", 0, 14));
+        jMenuItem_clientes.setIcon(new javax.swing.ImageIcon("C:\\Documents and Settings\\Kelly\\Meus documentos\\NetBeansProjects\\Auto-Pecas\\imagens\\botoes\\cliente.gif")); // NOI18N
+        jMenuItem_clientes.setText("Cadastro de Clientes");
+        jMenu_cadastros.add(jMenuItem_clientes);
 
         jMenuBar1.add(jMenu_cadastros);
 
+        jMenu_sair.setBackground(new java.awt.Color(0, 204, 0));
         jMenu_sair.setText("Sair");
         jMenu_sair.addMouseListener(new java.awt.event.MouseAdapter() {
             public void mouseClicked(java.awt.event.MouseEvent evt) {
@@ -136,19 +149,19 @@ public class Tela_Principal extends javax.swing.JFrame {
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(47, 47, 47)
-                .addComponent(jLabel_data)
-                .addGap(33, 33, 33)
-                .addComponent(jLabel_hora)
-                .addContainerGap(509, Short.MAX_VALUE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jLabel_data)
+                    .addComponent(jLabel_hora))
+                .addContainerGap(603, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addContainerGap(554, Short.MAX_VALUE)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jLabel_data)
-                    .addComponent(jLabel_hora))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap(536, Short.MAX_VALUE)
+                .addComponent(jLabel_data)
+                .addGap(4, 4, 4)
+                .addComponent(jLabel_hora)
                 .addContainerGap())
             .addComponent(jPanel1, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
@@ -173,6 +186,11 @@ public class Tela_Principal extends javax.swing.JFrame {
         System.exit(0);
 }//GEN-LAST:event_jButton_sairActionPerformed
 
+    private void jMenuItem_cad_produtosMousePressed(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jMenuItem_cad_produtosMousePressed
+        //evento do menuItem_cad_produtos
+        new Cad_produto().show();
+    }//GEN-LAST:event_jMenuItem_cad_produtosMousePressed
+
     /**
     * @param args the command line arguments
     */
@@ -185,14 +203,14 @@ public class Tela_Principal extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton jButton_clientes;
+    private javax.swing.JButton jButton1;
     private javax.swing.JButton jButton_produtos;
     private javax.swing.JButton jButton_sair;
     private javax.swing.JLabel jLabel_data;
     private javax.swing.JLabel jLabel_hora;
     private javax.swing.JMenuBar jMenuBar1;
-    private javax.swing.JMenuItem jMenuItem1;
-    private javax.swing.JMenuItem jMenuItem2;
+    private javax.swing.JMenuItem jMenuItem_cad_produtos;
+    private javax.swing.JMenuItem jMenuItem_clientes;
     private javax.swing.JMenu jMenu_cadastros;
     private javax.swing.JMenu jMenu_sair;
     private javax.swing.JPanel jPanel1;
